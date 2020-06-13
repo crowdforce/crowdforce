41601c855606140b169cb4e45220fadcc0bded0b
sausageRoll <drozdovigor1996@gmail.com>
2020-06-13 15:39:22 +0200

    Fix code generation

--- cf-app/src/main/kotlin/space/crowdforce/service/CodesGeneratorService.kt
+++ cf-app/src/main/kotlin/space/crowdforce/service/CodesGeneratorService.kt
@@ -3,19 +3,18 @@ package space.crowdforce.service
 import org.springframework.stereotype.Service
 import space.crowdforce.repository.UserCodesRepository
 import space.crowdforce.tables.records.UserCodesRecord
-import java.nio.ByteBuffer
 import java.security.SecureRandom
+import kotlin.random.asKotlinRandom
 
 @Service
 class CodesGeneratorService(
     private val userCodesRepository: UserCodesRepository
 ) {
 
-    val secureRandom = SecureRandom()
+    val secureRandom = SecureRandom().asKotlinRandom()
 
     fun generateCode(userId: Int): UserCodesRecord =
-        secureRandom.generateSeed(6)
-            .let { ByteBuffer.wrap(it).int }
+        secureRandom.nextInt(100000, 999999)
             .let {
                 userCodesRepository.insertNewUserCode(userId, it)
             }
