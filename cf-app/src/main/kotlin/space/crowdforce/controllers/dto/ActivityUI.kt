0b8236f3aef730db8a75d0eb995c1948c21a8832
mstepachev <maksim.stepachev@gmail.com>
2020-06-20 03:00:52 +0300

    Project controller

---
+++ cf-app/src/main/kotlin/space/crowdforce/controllers/dto/ActivityUI.kt
@@ -0,0 +1,13 @@
+package space.crowdforce.controllers.dto
+
+import java.time.LocalDateTime
+
+data class ActivityUI(
+        val id: Int,
+        val name: String,
+        val lat: Float,
+        val lng: Float,
+        val startDate: LocalDateTime,
+        val endDate: LocalDateTime,
+        val isSubscribed: Boolean
+);
