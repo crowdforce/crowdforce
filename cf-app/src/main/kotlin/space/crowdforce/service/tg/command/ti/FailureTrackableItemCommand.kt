47e02f5b17d04ccc0e8c0317d6e629524605be4c
mstepachev <maksim.stepachev@gmail.com>
2021-07-09 12:57:47 +0300

    TG bot trackable item interaction.

--- cf-app/src/main/kotlin/space/crowdforce/service/tg/command/ti/FailureTrackableItemCommand.kt
+++ cf-app/src/main/kotlin/space/crowdforce/service/tg/command/ti/FailureTrackableItemCommand.kt
@@ -1,26 +1,31 @@
 package space.crowdforce.service.tg.command.ti
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import space.crowdforce.domain.User
 import space.crowdforce.domain.item.ConfirmationStatus
+import space.crowdforce.repository.TrackableItemEventParticipantRepository
 import space.crowdforce.service.tg.Argument
 import space.crowdforce.service.tg.UserContext
 import space.crowdforce.service.tg.command.Command
 import space.crowdforce.service.tg.command.CommandAnswer
-import space.crowdforce.service.ti.TrackableItemService
+import java.time.Clock
+import java.time.LocalDateTime
 
 @Service
 class FailureTrackableItemCommand(
-    private val trackableItemService: TrackableItemService
+    private val trackableItemEventParticipantRepository: TrackableItemEventParticipantRepository,
+    private val clock: Clock
 ) : Command {
     override fun name(): String = NAME
 
     override fun arguments(): List<Argument> = listOf(Argument.TRACKABLE_ITEM_EVENT_ID)
 
+    @Transactional
     override fun execute(user: User, context: UserContext): CommandAnswer {
         val eventId = context.value(Argument.TRACKABLE_ITEM_EVENT_ID)!!.toInt()
 
-        trackableItemService.changeParticipantEventStatus(user.id, eventId, ConfirmationStatus.COMPLETED)
+        trackableItemEventParticipantRepository.updateStatus(user.id, eventId, ConfirmationStatus.COMPLETED, LocalDateTime.now(clock))
 
         // TODO remove previous message after approve
         return CommandAnswer.finish(
